dbe2ad747be9bdfa5e13acc0d8a70c7f4908311f
location67ha-cyber <location67ha@gmail.com>
2026-07-17 19:13:40 +0200

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -3,8 +3,8 @@
 // Remplacer par vos vraies clés
 // ============================================
 
-const SUPABASE_URL = 'https://VOTRE_PROJECT_ID.supabase.co'
-const SUPABASE_ANON_KEY = 'VOTRE_ANON_KEY'
+const SUPABASE_URL = 'https://jkovlclyumqfbkbekrel.supabase.co/'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imprb3ZsY2x5dW1xZmJrYmVrcmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzgzMzAsImV4cCI6MjA5OTg1NDMzMH0.wtnOUl97eYRfnhbNKRGpsm-Z3tD6qC2SJOk1qUAKIEc'
 
 const { createClient } = supabase
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
